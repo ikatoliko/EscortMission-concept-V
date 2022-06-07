bbef2e5df2fc730a04685f4003ffedca3bfceeba
ikatoliko <37219993+ikatoliko@users.noreply.github.com>
2022-06-07 14:15:55 +0200

    Created EquipmentQualitySO and added it to EquipmentSO. Created example weapon (Crude Iron sword) and scriptable objects necessary to it

--- EscortMission-concept-V/Assets/ScriptableObjects/Items/Equipment/EquipmentSO.cs
+++ EscortMission-concept-V/Assets/ScriptableObjects/Items/Equipment/EquipmentSO.cs
@@ -2,4 +2,5 @@ using UnityEngine;
 
 public abstract class EquipmentSO : BaseSO {
     [field: SerializeField] public EquipmentMaterialSO Material { get; private set; }
+    [field: SerializeField] public EquipmentQualitySO Quality { get; private set; }
 }
---
+++ EscortMission-concept-V/Assets/ScriptableObjects/Items/Equipment/Qualities/EquipmentQualitySO.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment Quality", menuName = "SOs/Items/Equipment/Quality")]
+public class EquipmentQualitySO : BaseSO {
+}
